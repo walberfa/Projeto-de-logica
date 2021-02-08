bcd11ef241fdb1fcadccc5ee677d400c64babee3
Helaine <helaine.alves.pontes07@aluno.ifce.edu.br>
2021-02-08 18:32:47 -0300

    Manipulacao de string finalizada

--- alocacao.py
+++ alocacao.py
@@ -1,35 +1,124 @@
 from formula import *
-from funcs import*
-aulas = [
-			{
-				"s1": [
-					{
-						"professor": "Carlos",
-						"disciplina": "Circuitos Digitais"
-					},
-					{
-						"professor": "Daniel",
-						"disciplina": "Fundamentos da Programação"
-					}
-				]
-			},
-			{
-				"s2": [
-					{
-						"professor": "Carlos",
-						"disciplina": "Laboratório de Programação"
-					},
-					{
-						"professor": "Walber",
-						"disciplina": "Entenda sobre o BBB"
-					},
-					{
-						"professor": "Helaine",
-						"disciplina": "Como ser bonita"
-					}
-				]
-			}
-		]
+
+
+string = """s1
+Circuitos Digitais, Carlos
+Fundamentos de Programação, Daniel
+s2
+Laboratório de Programação, Samuel
+s3
+POO, Marcio
+Programação Linear, Ana
+s4
+Lógica para Computaçãp, João
+Comunicação de Dados, Jos ́e
+Estruturas de Dados, Tomás
+s5
+Análise de Algoritmos, Alice
+Grafos, Alice
+Sistemas Operacionais, Carlos
+Eletrônica, Santos
+Engenharia de Software, Marcio
+s6
+Linguagens de Programação, Fernanda
+Teoria da Computação, João
+Sistemas Embarcados, Santos
+Redes, Tomás"""
+
+
+
+def stringToDict(entrada):
+    professor= []
+    semestre=[]
+    cadeira=[]
+    separa = string.split()
+    contador=0
+    quant_cadeiras = []
+    arry_prof=[]
+    dict_semestr = {}
+    array_semestr = []
+    dict_disciplin = {}
+    sem=0
+    prof=0
+
+    #separa os professores
+    for palavras in separa:
+        contador=contador+1
+        if palavras[-1]==',':
+            professor.append(separa[contador])
+    #print(professor)
+
+    #sepra os semestres
+    semestre= string.split('\n')
+    for p in professor:
+        for palavras2 in semestre:
+            if p in palavras2:
+                semestre.remove(palavras2)
+        contador+=1
+    #print(semestre)
+
+    #separa as cadeiras
+    separa=string.split(',')
+    axl=[]
+    contador=0
+
+    for d in separa:
+        axl.append(separa[contador].split('\n'))
+        contador+=1
+    for h in axl:
+        for j in h:
+            cadeira.append(j)
+    for a in cadeira:
+        for pro in professor:
+            if a == ' '+pro :
+                cadeira.remove(a)
+
+    for a in cadeira:
+        for pro in semestre:
+            if a == pro :
+                cadeira.remove(a)
+
+    #quantas cadeiras tem em cada semestre
+    separa= string.split()
+    x=0
+    axl=[]
+    for se in semestre[::-1]:
+        for sep in separa[::-1]:
+            if ',' in sep:
+                x+=1
+            if se == sep:
+                for qt in axl:
+                    x=x-qt
+                axl.append(x)
+        x=0
+    for ax in axl[::-1]:
+        quant_cadeiras.append(ax)
+
+    for se in range(len(semestre)):
+        cad =int(quant_cadeiras[se])
+        sem=0
+        while sem != cad :
+            dic_cadeiras={'professor': professor[prof],'disciplina' : cadeira[prof]}
+            arry_prof.append(dic_cadeiras)
+            sem+=1
+            prof+=1
+        array_semestr.append({semestre[se]:arry_prof})
+        arry_prof=[]
+    return array_semestr
+
+def and_all(formula):
+	formula_final = formula[0]
+	del formula[0]
+	for form in formula:
+		formula_final = And(formula_final, form)
+	return formula_final
+
+def or_all(formula):
+	formula_final = formula[0]
+	del formula[0]
+	for form in formula:
+		formula_final = Or(formula_final, form)
+	return formula_final
 
 def and_all(formula):
 	formula_final = formula[0]
@@ -102,5 +191,7 @@ def aulas_horario_or(aulas):
 											Atom(str(list(semestreAtual.keys())[0]) + "_" + str(horario) + "_" + profAtual["professor"] + "_" + profAtual["disciplina"])))	
 	return or_all(restricoes)
 
+aulas = stringToDict(string)
 formula_disciplina_horario = And(aulas_horario_or(aulas), aulas_horario_not_and(aulas))
 final_formula = And(And(professor_horario(aulas), disciplina_horario(aulas)), formula_disciplina_horario)
+print(final_formula)
